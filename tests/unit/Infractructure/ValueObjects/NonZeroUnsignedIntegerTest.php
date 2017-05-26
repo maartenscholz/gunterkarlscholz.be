d85232493ca46f20d8b7652bf349cba15fe56556
Maarten Scholz <maarten.scholz@gmail.com>
2017-05-26 02:22:20 +0200

    Added NonZeroUnsignedInteger unit test

---
+++ tests/unit/Infractructure/ValueObjects/NonZeroUnsignedIntegerTest.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Gks\Tests\Unit\Infrastructure\ValueObjects;
+
+use Gks\Infrastructure\ValueObjects\NonZeroUnsignedInteger;
+use InvalidArgumentException;
+use PHPUnit\Framework\TestCase;
+
+class NonZeroUnsignedIntegerTest extends TestCase
+{
+    /**
+     * @return array
+     */
+    public function invalidValues()
+    {
+        return [
+            [0],
+            [-1],
+            [-100],
+        ];
+    }
+
+    /**
+     * @test
+     * @dataProvider invalidValues
+     *
+     * @param int $value
+     */
+    public function it_throws_when_constructed_with_invalid_values(int $value)
+    {
+        $this->expectException(InvalidArgumentException::class);
+
+        new NonZeroUnsignedInteger($value);
+    }
+
+    /**
+     * @test
+     */
+    public function it_can_return_its_value()
+    {
+        $integer = new NonZeroUnsignedInteger(1);
+
+        $this->assertSame(1, $integer->getValue());
+    }
+
+    /**
+     * @test
+     */
+    public function it_can_be_cast_to_a_string()
+    {
+        $integer = new NonZeroUnsignedInteger(2);
+
+        $this->assertSame('2', (string) $integer);
+    }
+}
