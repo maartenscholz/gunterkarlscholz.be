1787e2bb276c402dbc2d72cf9ab51c7bfc09f207
Maarten Scholz <maarten.scholz@gmail.com>
2018-04-08 15:11:08 +0200

    Changes needed to implement new route package

--- src/Application/Providers/RouteServiceProvider.php
+++ src/Application/Providers/RouteServiceProvider.php
@@ -8,14 +8,15 @@ use Gks\Application\Http\Controllers\Admin\WorksController;
 use Gks\Application\Http\Controllers\HomeController;
 use Gks\Application\Http\Controllers\ImagesController;
 use Gks\Application\Http\Middleware\AuthorizationMiddleware;
+use Gks\Application\Http\Middleware\CsrfMiddleware;
 use Gks\Application\Http\Middleware\GuestMiddleware;
-use Gks\Application\Http\MiddlewareStrategy;
 use Gks\Application\Http\RequestHandlers\Admin\LoginPageRequestHandler;
 use Gks\Application\Http\RequestHandlers\Admin\LoginRequestHandler;
 use Gks\Application\Http\RequestHandlers\Admin\LogoutRequestHandler;
 use Gks\Application\Http\RequestHandlers\Admin\Works;
 use League\Container\ServiceProvider\AbstractServiceProvider;
 use League\Route\RouteCollection;
+use League\Route\RouteGroup;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Zend\Diactoros\Response;
@@ -32,17 +33,6 @@ class RouteServiceProvider extends AbstractServiceProvider
         ServerRequestInterface::class,
         ResponseInterface::class,
         RouteCollection::class,
-        MiddlewareStrategy::class,
-    ];
-
-    /**
-     * @var array
-     */
-    protected $routeMiddleware = [
-        'session.create' => [GuestMiddleware::class],
-        'session.store' => [GuestMiddleware::class],
-        'session.destroy' => [AuthorizationMiddleware::class],
-        'admin.*' => [AuthorizationMiddleware::class],
     ];
 
     /**
@@ -72,34 +62,34 @@ class RouteServiceProvider extends AbstractServiceProvider
 
         $this->container->share(RouteCollection::class, function () {
             $route = new RouteCollection($this->container);
-            $route->setStrategy($this->container->get(MiddlewareStrategy::class));
+
+            $route->middleware($this->container->get(CsrfMiddleware::class));
 
             $route->get('/', [HomeController::class, 'index'])->setName('home');
-            $route->get('/login', $this->container->get(LoginPageRequestHandler::class))->setName('session.create');
-            $route->post('/login', $this->container->get(LoginRequestHandler::class))->setName('session.store');
-            $route->get('/logout', $this->container->get(LogoutRequestHandler::class))->setName('session.destroy');
 
-            $route->get('image/{path}', [ImagesController::class, 'show'])->setName('images.show');
+            $route->get('/login', $this->container->get(LoginPageRequestHandler::class))
+                ->middleware($this->container->get(GuestMiddleware::class));
+            $route->post('/login', $this->container->get(LoginRequestHandler::class))
+                ->middleware($this->container->get(GuestMiddleware::class));
+            $route->get('/logout', $this->container->get(LogoutRequestHandler::class))
+                ->middleware($this->container->get(AuthorizationMiddleware::class));
 
-            $route->get('/admin', [DashboardController::class, 'index'])->setName('admin.dashboard');
-            $route->get('/admin/works', $this->container->get(Works\IndexRequestHandler::class))->setName('admin.works.index');
-            $route->get('/admin/works/create', $this->container->get(Works\AddRequestHandler::class))->setName('admin.works.create');
-            $route->post('/admin/works', [WorksController::class, 'store'])->setName('admin.works.store');
-            $route->get('/admin/works/{id}/edit', $this->container->get(Works\EditRequestHandler::class))->setName('admin.works.edit');
-            $route->put('/admin/works/{id}', [WorksController::class, 'update'])->setName('admin.works.update');
-            $route->get('/admin/works/{id}/destroy', [WorksController::class, 'destroy'])->setName('admin.works.destroy');
-            $route->get('/admin/works/{id}/images', [WorkImagesController::class, 'index'])->setName('admin.works.images.index');
-            $route->post('/admin/works/{id}/images', $this->container->get(Works\StoreImageRequestHandler::class))->setName('admin.works.images.store');
-            $route->post('/admin/works/{work_id}/images/{image_id}', $this->container->get(Works\RemoveImageRequestHandler::class))->setName('admin.images.destroy');
+            $route->get('/image/{path}', [ImagesController::class, 'show'])->setName('images.show');
+
+            $route->group('/admin', function (RouteGroup $route) {
+                $route->get('/', [DashboardController::class, 'index']);
+                $route->get('/works', $this->container->get(Works\IndexRequestHandler::class));
+                $route->get('/works/create', $this->container->get(Works\AddRequestHandler::class));
+                $route->post('/works', [WorksController::class, 'store'])->setName('admin.works.store');
+                $route->get('/works/{id}/edit', $this->container->get(Works\EditRequestHandler::class));
+                $route->put('/works/{id}', [WorksController::class, 'update']);
+                $route->get('/works/{id}/destroy', [WorksController::class, 'destroy']);
+                $route->get('/works/{id}/images', [WorkImagesController::class, 'index']);
+                $route->post('/works/{id}/images', $this->container->get(Works\StoreImageRequestHandler::class));
+                $route->post('/works/{work_id}/images/{image_id}', $this->container->get(Works\RemoveImageRequestHandler::class));
+            })->middleware($this->container->get(AuthorizationMiddleware::class));
 
             return $route;
-        });
-
-        $this->container->share(MiddlewareStrategy::class, function () {
-            $strategy = new MiddlewareStrategy($this->routeMiddleware);
-            $strategy->setContainer($this->container);
-
-            return $strategy;
         });
     }
 }
