69a6a83d825f7f7d8bb504da02bbe54a159c83c1
Maarten Scholz <maarten.scholz@gmail.com>
2018-08-18 00:36:22 +0200

    Check if $debugBar exists

--- resources/templates/admin/app.php
+++ resources/templates/admin/app.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-beta/css/materialize.min.css">
-        <?php if ($debugBar): ?>
+        <?php if (isset($debugBar)): ?>
             <link rel="stylesheet" href="/debugbar/css">
         <?php endif; ?>
         <title><?= $title ? $this->e($title).' | ' : '' ?>GKS</title>
@@ -29,7 +29,7 @@
                 integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
                 crossorigin="anonymous"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-beta/js/materialize.min.js"></script>
-        <?php if ($debugBar): ?>
+        <?php if (isset($debugBar)): ?>
             <script type="text/javascript" src="/debugbar/js"></script>
             <script type="text/javascript">jQuery.noConflict(true);</script>
             <?= $debugBar->render() ?>
