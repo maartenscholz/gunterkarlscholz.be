2ee7659b294d573f24aa5dac370e3539ec23ee10
Maarten Scholz <maarten@madewithlove.be>
2019-04-06 02:08:28 +0200

    Update add work validator

--- src/Infrastructure/UserInterface/Http/RequestHandlers/Admin/Works/StoreRequestHandler.php
+++ src/Infrastructure/UserInterface/Http/RequestHandlers/Admin/Works/StoreRequestHandler.php
@@ -5,9 +5,11 @@ namespace Gks\Infrastructure\UserInterface\Http\RequestHandlers\Admin\Works;
 use Aura\Session\Segment;
 use Gks\Application\Commands\AddWork;
 use Gks\Domain\Model\Works\Type;
+use Gks\Infrastructure\UserInterface\Http\Validation\MatchNotEmpty;
 use League\Tactician\CommandBus;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
+use Sirius\Validation\Rule\GreaterThan;
 use Sirius\Validation\Rule\InList;
 use Sirius\Validation\Rule\Required;
 use Sirius\Validation\Validator;
@@ -57,6 +59,10 @@ final class StoreRequestHandler
         $validator->add('title[en_US]', Required::class);
         $validator->add('title[fr_FR]', Required::class);
         $validator->add('title[de_DE]', Required::class);
+        $validator->add('width', GreaterThan::class, [GreaterThan::OPTION_MIN => 0]);
+        $validator->add('height', GreaterThan::class, [GreaterThan::OPTION_MIN => 0, GreaterThan::OPTION_INCLUSIVE]);
+        $validator->add('width', MatchNotEmpty::class, [MatchNotEmpty::OPTION_ITEM => 'height']);
+        $validator->add('height', MatchNotEmpty::class, [MatchNotEmpty::OPTION_ITEM => 'width']);
 
         return $validator;
     }
