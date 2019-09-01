86c9e2001c65b5b65a8c4e71298ba912963d8780
Maarten Scholz <maarten@madewithlove.be>
2019-09-01 22:22:11 +0200

    Renamed css also from app to splash

--- resources/templates/splash.php
+++ resources/templates/splash.php
@@ -4,7 +4,7 @@
         <meta charset="UTF-8">
         <link rel="stylesheet" type="text/css"
               href="https://cdnjs.cloudflare.com/ajax/libs/normalize/4.2.0/normalize.min.css">
-        <link rel="stylesheet" type="text/css" href="css/app.css">
+        <link rel="stylesheet" type="text/css" href="css/splash.css">
         <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
         <title><?= isset($title) ? $this->e($title).' | ' : '' ?>Gunter-Karl Scholz</title>
     </head>
