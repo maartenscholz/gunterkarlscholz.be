38028aca35be04a0e114daa2f3f1dab603994438
Maarten Scholz <maarten@madewithlove.be>
2019-10-13 09:33:45 +0200

    Restructured work overview

--- resources/templates/works/index.php
+++ resources/templates/works/index.php
@@ -5,15 +5,15 @@
     'title' => 'Portfolio',
 ]) ?>
 
-<ul class="work-overview">
+<section class="work-overview">
     <?php foreach ($works as $work): ?>
-        <li>
+        <article class="work-overview__item">
             <?php if (!empty($work->getImages())): ?>
                 <img
                         src="/image<?= $imageUrlBuilder->getUrl($work->getImages()[0]->getFilename(), ['w' => 500, 'h' => 500, 'fit' => 'crop']) ?>"
                         alt=""
                 >
             <?php endif; ?>
-        </li>
+        </article>
     <?php endforeach; ?>
-</ul>
+</section>
