d1ce218456dba91674dbb6be56db5b0158b0f974
Maarten Scholz <maarten@madewithlove.be>
2019-10-13 11:53:41 +0200

    Lazy load route request handlers

--- src/Infrastructure/UserInterface/Http/RouteServiceProvider.php
+++ src/Infrastructure/UserInterface/Http/RouteServiceProvider.php
@@ -12,7 +12,7 @@ use League\Route\Router;
 use League\Route\Strategy\ApplicationStrategy;
 use Zend\Diactoros\Response;
 
-class RouteServiceProvider extends AbstractServiceProvider
+final class RouteServiceProvider extends AbstractServiceProvider
 {
     /**
      * @var Container
@@ -38,58 +38,40 @@ class RouteServiceProvider extends AbstractServiceProvider
 
                 $route->middleware($this->container->get(Middleware\CsrfMiddleware::class));
 
-                $route->get('/', $this->container->get(RequestHandlers\HomeRequestHandler::class));
+                $route->get('/', RequestHandlers\HomeRequestHandler::class);
 
-                $route->get('/login', $this->container->get(RequestHandlers\Admin\LoginPageRequestHandler::class))
+                $route->get('/login', RequestHandlers\Admin\LoginPageRequestHandler::class)
                     ->middleware($this->container->get(Middleware\GuestMiddleware::class));
-                $route->post('/login', $this->container->get(RequestHandlers\Admin\LoginRequestHandler::class))
+                $route->post('/login', RequestHandlers\Admin\LoginRequestHandler::class)
                     ->middleware($this->container->get(Middleware\GuestMiddleware::class));
-                $route->get('/logout', $this->container->get(RequestHandlers\Admin\LogoutRequestHandler::class))
+                $route->get('/logout', RequestHandlers\Admin\LogoutRequestHandler::class)
                     ->middleware($this->container->get(Middleware\AuthorizationMiddleware::class));
 
-                $route->get('/image/{path}', $this->container->get(RequestHandlers\ServeImageRequestHandler::class));
+                $route->get('/image/{path}', RequestHandlers\ServeImageRequestHandler::class);
 
-                $route->get('/portfolio', $this->container->get(RequestHandlers\Works\Index::class));
+                $route->get('/portfolio', RequestHandlers\Works\Index::class);
 
                 $route->group(
                     '/admin',
                     function (RouteGroup $route) {
-                        $route->get('/', $this->container->get(RequestHandlers\Admin\DashboardRequestHandler::class));
-                        $route->get(
-                            '/works',
-                            $this->container->get(RequestHandlers\Admin\Works\IndexRequestHandler::class)
-                        );
-                        $route->get(
-                            '/works/create',
-                            $this->container->get(RequestHandlers\Admin\Works\AddRequestHandler::class)
-                        );
-                        $route->post(
-                            '/works',
-                            $this->container->get(RequestHandlers\Admin\Works\StoreRequestHandler::class)
-                        );
-                        $route->get(
-                            '/works/{id}/edit',
-                            $this->container->get(RequestHandlers\Admin\Works\EditRequestHandler::class)
-                        );
-                        $route->put(
-                            '/works/{id}',
-                            $this->container->get(RequestHandlers\Admin\Works\UpdateRequestHandler::class)
-                        );
-                        $route->get(
-                            '/works/{id}/destroy',
-                            $this->container->get(RequestHandlers\Admin\Works\DestroyRequestHandler::class)
-                        );
+                        $route->get('/', RequestHandlers\Admin\DashboardRequestHandler::class);
+                        $route->get('/works', RequestHandlers\Admin\Works\IndexRequestHandler::class);
+                        $route->get('/works/create', RequestHandlers\Admin\Works\AddRequestHandler::class);
+                        $route->post('/works', RequestHandlers\Admin\Works\StoreRequestHandler::class);
+                        $route->get('/works/{id}/edit', RequestHandlers\Admin\Works\EditRequestHandler::class);
+                        $route->put('/works/{id}', RequestHandlers\Admin\Works\UpdateRequestHandler::class);
+                        $route->get('/works/{id}/destroy', RequestHandlers\Admin\Works\DestroyRequestHandler::class);
                         $route->get(
                             '/works/{id}/images',
-                            $this->container->get(RequestHandlers\Admin\Works\Images\IndexRequestHandler::class)
+                            RequestHandlers\Admin\Works\Images\IndexRequestHandler::class
                         );
                         $route->post(
                             '/works/{id}/images',
-                            $this->container->get(RequestHandlers\Admin\Works\Images\StoreRequestHandler::class)
+                            RequestHandlers\Admin\Works\Images\StoreRequestHandler::class
                         );
                         $route->post(
                             '/works/{work_id}/images/{image_id}',
-                            $this->container->get(RequestHandlers\Admin\Works\Images\RemoveRequestHandler::class)
+                            RequestHandlers\Admin\Works\Images\RemoveRequestHandler::class
                         );
                     }
                 )->middleware($this->container->get(Middleware\AuthorizationMiddleware::class));
