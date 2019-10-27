c8136de54b001afdaa80a7d30f140080c55a8ad3
Maarten Scholz <maarten@madewithlove.be>
2019-10-27 14:29:38 +0100

    Use absolute link for css

--- resources/templates/app.php
+++ resources/templates/app.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link rel="stylesheet" type="text/css"
               href="https://cdnjs.cloudflare.com/ajax/libs/normalize/4.2.0/normalize.min.css">
-        <link rel="stylesheet" href="css/app.css">
+        <link rel="stylesheet" href="/css/app.css">
         <title><?= $title ? $this->e($title).' | ' : '' ?>GKS</title>
     </head>
     <body>
