3e563d5adf3f9fa2f489c6af062c3ec9559163fd
Maarten Scholz <maarten@madewithlove.be>
2019-11-10 15:01:31 +0100

    Add the work title in other languages below the main title

--- resources/templates/works/show.php
+++ resources/templates/works/show.php
@@ -6,7 +6,12 @@
 ]) ?>
 
 <section class="work-detail">
-    <h1><?= $work->getTitle()->getValue('en_US') ?></h1>
+    <h1 class="work-detail__title"><?= $work->getTitle()->getValue('en_US') ?></h1>
+    <h2 class="work-detail__subtitle">
+        <?= $work->getTitle()->getValue('nl_BE') ?> /
+        <?= $work->getTitle()->getValue('fr_FR') ?> /
+        <?= $work->getTitle()->getValue('de_DE') ?>
+    </h2>
     <div class="work-detail__image">
         <img src="/image<?= $imageUrlBuilder->getUrl($work->getImages()[0]->getFilename()) ?>" alt="">
     </div>
