c63aef8294bbefca2b3e248505efbc6fe5f9de6b
Maarten Scholz <maarten.scholz@flexmail.be>
2020-09-20 10:40:44 +0200

    Refactor service provider imports

--- bootstrap.php
+++ bootstrap.php
@@ -1,46 +1,35 @@
 <?php
 
 use Gks\Application;
-use Gks\Application\CommandBus\ServiceProvider as CommandBusServiceProvider;
-use Gks\Application\Handlers\ServiceProvider as CommandHandlerServiceProvider;
 use Gks\Infrastructure;
-use Gks\Infrastructure\Caching\Redis\ServiceProvider as RedisServiceProvider;
-use Gks\Infrastructure\Filesystem\ServiceProvider as FilesystemServiceProvider;
-use Gks\Infrastructure\Http\ServiceProvider as HttpServiceProvider;
-use Gks\Infrastructure\Logging\ServiceProvider;
-use Gks\Infrastructure\Persistence\MySQL\ServiceProvider as MySQLServiceProvider;
-use Gks\Infrastructure\Persistence\ServiceProvider as PersistenceServiceProvider;
-use Gks\Infrastructure\UserInterface\Http\ErrorHandling\ServiceProvider as ErrorHandlingServiceProvider;
-use Gks\Infrastructure\UserInterface\Http\GlideServiceProvider;
-use Gks\Infrastructure\UserInterface\Http\ServiceProvider as HTTPUserInterfaceServiceProvider;
-use Gks\Infrastructure\UserInterface\Http\SessionServiceProvider;
-use Gks\Infrastructure\UserInterface\Http\TemplatingServiceProvider;
 
 require __DIR__.'/vendor/autoload.php';
 
 $dotenv = new Dotenv\Dotenv(__DIR__);
 $dotenv->load();
-$dotenv->required([
-    'APP_ENV',
-    'APP_ADMIN_PASSWORD',
-    'GLIDE_SIGNATURE_KEY',
-    'SENTRY_DSN',
-    'REDIS_HOST',
-]);
+$dotenv->required(
+    [
+        'APP_ENV',
+        'APP_ADMIN_PASSWORD',
+        'GLIDE_SIGNATURE_KEY',
+        'SENTRY_DSN',
+        'REDIS_HOST',
+    ]
+);
 
-$container = new League\Container\Container;
+$container = new League\Container\Container();
 
-$container->addServiceProvider(ErrorHandlingServiceProvider::class);
-$container->addServiceProvider(SessionServiceProvider::class);
-$container->addServiceProvider(TemplatingServiceProvider::class);
-$container->addServiceProvider(CommandBusServiceProvider::class);
-$container->addServiceProvider(PersistenceServiceProvider::class);
-$container->addServiceProvider(GlideServiceProvider::class);
-$container->addServiceProvider(FilesystemServiceProvider::class);
-$container->addServiceProvider(ServiceProvider::class);
-$container->addServiceProvider(CommandHandlerServiceProvider::class);
-$container->addServiceProvider(HTTPUserInterfaceServiceProvider::class);
-$container->addServiceProvider(RedisServiceProvider::class);
-$container->addServiceProvider(MySQLServiceProvider::class);
-$container->addServiceProvider(HttpServiceProvider::class);
+$container->addServiceProvider(Application\CommandBus\ServiceProvider::class);
+$container->addServiceProvider(Application\Handlers\ServiceProvider::class);
+$container->addServiceProvider(Infrastructure\UserInterface\Http\ErrorHandling\ServiceProvider::class);
+$container->addServiceProvider(Infrastructure\UserInterface\Http\SessionServiceProvider::class);
+$container->addServiceProvider(Infrastructure\UserInterface\Http\TemplatingServiceProvider::class);
+$container->addServiceProvider(Infrastructure\UserInterface\Http\GlideServiceProvider::class);
+$container->addServiceProvider(Infrastructure\UserInterface\Http\ServiceProvider::class);
+$container->addServiceProvider(Infrastructure\Persistence\ServiceProvider::class);
+$container->addServiceProvider(Infrastructure\Persistence\MySQL\ServiceProvider::class);
+$container->addServiceProvider(Infrastructure\Filesystem\ServiceProvider::class);
+$container->addServiceProvider(Infrastructure\Logging\ServiceProvider::class);
+$container->addServiceProvider(Infrastructure\Caching\Redis\ServiceProvider::class);
+$container->addServiceProvider(Infrastructure\Http\ServiceProvider::class);
 $container->addServiceProvider(Infrastructure\Events\ServiceProvider::class);
