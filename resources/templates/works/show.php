0475114bd31ab51dcdedba53f45634ccdb275d80
Maarten Scholz <maarten.scholz@flexmail.be>
2020-11-15 12:20:37 +0100

    Add description to contact detail page

--- resources/templates/works/show.php
+++ resources/templates/works/show.php
@@ -5,13 +5,18 @@
     'title' => $work->title->nl . ' | Portfolio',
 ]) ?>
 
-<section class="work-detail">
+<section class="work-detail<?= $work->description ? '' : ' work-detail--no-description' ?>">
     <h1 class="work-detail__title"><?= $work->title->nl ?></h1>
     <div class="work-detail__translations">
         <?= $work->title->en ?> /
         <?= $work->title->fr ?> /
         <?= $work->title->de ?>
     </div>
+    <?php if ($work->description): ?>
+        <div class="work-detail__description">
+            <?= nl2br($this->escape($work->description)) ?>
+        </div>
+    <?php endif; ?>
     <div class="work-detail__image">
         <img src="/image<?= $imageUrlBuilder->getUrl($work->images[0]->filename) ?>" alt="">
     </div>
